95bfaa0a41a229ff534d964da284d21e9737c86f
sky3d <sky3ddd@gmail.com>
2021-12-25 11:31:02 +0300

    chore: add commitlint, husky

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,12 @@ import fastify from 'fastify'
 
 const server = fastify({ logger: true })
 
-server.get('/ping', async (request, reply) => {
-    return 'pong\n'
-})
+server.get('/ping', async (request, reply) => 'pong\n')
 
 server.listen(3000, (err, address) => {
-    if (err) {
-        console.error(err)
-        process.exit(1)
-    }
-    console.log(`Server listening at ${address}`)
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
 })

79eb5f5ae862a49b6872a6f92040aacb53c8af08
sky3d <sky3ddd@gmail.com>
2021-12-28 17:17:48 +0300

    ci: remove client checking

--- server/src/handlers/rengaCreate.ts
+++ server/src/handlers/rengaCreate.ts
@@ -1,10 +1,9 @@
-import { create } from 'domain'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import * as shortid from 'shortid'
 import { Renku } from '../interfaces'
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const handler = async (service: Renga, request: FastifyRequest, reply: FastifyReply) => {
+const handler = async (service: Renga, request: FastifyRequest, _: FastifyReply) => {
   const payload = request.body as Renku
 
   request.log.info({ body: request.body }, 'renga.create request')

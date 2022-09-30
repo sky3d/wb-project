6a7f860d051827236a1a5ddca4ed94ea84cbc765
sky3d <sky3ddd@gmail.com>
2022-09-30 16:32:24 +0300

    fix: renga verse list route param

--- server/src/api/routes/verse/list.ts
+++ server/src/api/routes/verse/list.ts
@@ -6,7 +6,7 @@ import { handler } from '../../handlers/verse/list'
 export async function VerseList(fastify: FastifyInstance) {
   fastify.route({
     method: 'GET',
-    url: '/renga/{:id}/verses',
+    url: '/renga/:id/verses',
     schema,
     handler
   })
